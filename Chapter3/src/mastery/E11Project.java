c01ff73f0d174e9d9488200f1cea5f7c8e7601f6
78353001 <78353001@L815WD220031.edu.cbe.ab.ca>
2024-09-11 11:00:55 -0600

    11:00 AM

---
+++ Chapter3/src/mastery/E11Project.java
@@ -0,0 +1,20 @@
+package mastery;
+
+public class E11Project {
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Decimal		Binary		Hexadecimal");
+		
+		int a = 1
+		
+		System.out.println(" ");
+		
+	
+	
+	
+	
+	
+	}	
+}
---
+++ Chapter3/src/mastery/E6Digits.java
@@ -0,0 +1,19 @@
+package mastery;
+
+public class E6Digits {
+
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Tic Tac Toe rules");
+	System.out.println("Tic Tac Toe is a game for 2 players.");
+	System.out.println("1. A grid of 9 squares is drawn.");
+	System.out.println("2. The first player draws an X in an empty square.");
+	System.out.println("3. The second player draws an O in an empty square.");
+	System.out.println("4. Steps 2 and 3 are repeated until Tic Tac Pattern is created:");
+	System.out.println("	Patterns can be diagonal, horizontal, or vertical.");
+	System.out.println("5. A winning pattern is completed by drawing a line through the pattern.");
+	
+	}
+	
+}
