f897d0484c4d90cb1ad35ddec77085b9c214abda
78353001 <78353001@L815WD220031.edu.cbe.ab.ca>
2024-09-11 11:27:14 -0600

    11:27

--- Chapter3/src/mastery/E11Project.java
+++ Chapter3/src/mastery/E11Project.java
@@ -7,14 +7,19 @@ public class E11Project {
 		
 		System.out.println("Decimal		Binary		Hexadecimal");
 		
-		int a = 1
+		int a = 1;
+		String binary = Integer.toBinaryString(a);
+		String hex = Integer.toHexString(a);
+		System.out.println(a + "		" + binary + "		" + hex);
 		
-		System.out.println(" ");
+		for (int i = 0; i < 19; i++) {
 		
-	
-	
-	
-	
-	
+			a += 1;
+			binary = Integer.toBinaryString(a);
+			hex = Integer.toHexString(a);
+			System.out.println(a + "		" + binary + "		" + hex);
+		
+		}
+		
 	}	
 }
