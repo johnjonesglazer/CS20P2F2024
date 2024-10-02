31191a48b297e26b4fee0caf3d7d5188291ab17e
78353001 <78353001@L815WD220031.edu.cbe.ab.ca>
2024-10-02 11:57:01 -0600

    11:56

---
+++ Chapter7/src/skillbuilders/Circle.java
@@ -0,0 +1,27 @@
+package skillbuilders;
+
+public class Circle {
+	
+	private final double PI = 
+	
+	private double r;
+	
+	
+	public Circle()
+	{
+		r = 2.4;
+	}
+	
+	public Circle(double rad)
+	{
+		r = rad;
+	}
+	
+	public double circumference(){
+		
+		circum = (2* PI * r);
+		return;
+	}
+	
+
+}
