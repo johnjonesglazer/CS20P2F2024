7daee4e106668b910b0bc37a932005c2250db5bb
78353001 <78353001@L815WD220031.edu.cbe.ab.ca>
2024-10-15 10:14:48 -0600

    10:14

--- Chapter7/src/mastery/E2DigitExtractor.java
+++ Chapter7/src/mastery/E2DigitExtractor.java
@@ -10,7 +10,7 @@ Author: bilal
 School: CHHS
 Course: Computer Programming 20
 */
-
+import static java.lang.Math.abs;
 public class E2DigitExtractor {
 	private int wholeNumber; //making these variables private and unaccessable
 	private int onesPlace;
@@ -32,15 +32,36 @@ public class E2DigitExtractor {
 		hundredsPlace = h;
 	}
 	
-	public int showWhole(int input) {
-		int wholeNumber = input;
-		System.out.println("The whole number is " + wholeNumber + ".");
-		return(wholeNumber);
+	public int showWhole(int input) { //method to show whole
+		int wholeNumber = input; //accepts input 
+		System.out.println("The whole number is " + wholeNumber + "."); //prints whole number
+		return(wholeNumber); //returns whole number
+	}
+	
+	public int showOnesplace(int input) { //method to show ones place
+		int wholeNumber = input; //accepts input
+		int onesPlace = wholeNumber - ((wholeNumber / 10) * 10);// gets rid of everything except the ones place
+		onesPlace = abs(onesPlace); //converts to absolute value in case number happens to be negative
+		System.out.println("The ones place number is " + onesPlace + "."); //prints ones place
+		return(onesPlace);//returns ones place
 	}
 	
 	
+	public int showTensplace(int input) {//method to show tens place
+		int wholeNumber = input;//accepts input
+		int tensPlace = (wholeNumber / 10) - (wholeNumber / 100) * 10 ;// gets rid of everything except the tens place
+		tensPlace = abs(tensPlace); //converts to absolute value in case number happens to be negative
+		System.out.println("The tens place number is " + tensPlace + "."); //prints tens place
+		return(tensPlace);//returns tens place
+	}
 	
-	
+	public int showHundredsplace(int input) {//method to show hundreds place
+		int wholeNumber = input;//accepts input
+		int hundredsPlace = wholeNumber / 100;// gets rid of everything except the hundreds place
+		hundredsPlace = abs(hundredsPlace);  //converts to absolute value in case number happens to be negative
+		System.out.println("The hundreds place number is " + hundredsPlace + ".");//prints hundreds place
+		return(hundredsPlace); //returns hundreds place
+	}
 	
 	
 	
--- Chapter7/src/mastery/E2DigitExtractorTest.java
+++ Chapter7/src/mastery/E2DigitExtractorTest.java
@@ -13,7 +13,6 @@ School: CHHS
 Course: Computer Programming 20
 */
 
-import java.util.Scanner;
 public class E2DigitExtractorTest {
 	public static void main(String[] args){
 		//creating scanner for user input
@@ -24,75 +23,222 @@ public class E2DigitExtractorTest {
 				
 		//defining variables
 		int selection;
-		String haw = "w";
+		
 		
 	
 		while (true) { //loop incase user inputs something other then whats listed
 			try {
-			
 				System.out.println("Enter an integer.");
 				selection = Integer.parseInt(scanner.nextLine()); //accepts input
+				break;
 				
-				System.out.println("Show (W)hole number.");
+			}catch (Exception e) { //to not break code
+				System.out.println("Something went wrong. Please try again.");//tells user to try again
+				System.out.println("");//creates white space
+				continue;//restarts loop
+			}
+			
+				
+			
+		}
+			
+			
+			
+		while (true) {	
+			try {
+				
+				System.out.println("Show (W)hole number.");//instructions
 				System.out.println("Show (O)nes place number.");
 				System.out.println("Show (T)ens place number.");
 				System.out.println("Show (H)undreds place number.");
 				System.out.println("(Q)uit");
 				System.out.println("Enter your choice.");
-				String strselection =  scanner.nextLine().toLowerCase(); //accepts input
+				String strselection =  scanner.nextLine(); //accepts input
 				
-				
-				try {//in case a error happens the code doesnt just end
-					
-					
-				} catch (Exception e) { //to not break code
-					System.out.println("Something went wrong. Please try again.");//tells user to try again
+		
+		
+				if (strselection.equalsIgnoreCase("w")) {
+					num.showWhole(selection);//runs method from other file to show the number
 					System.out.println("");//creates white space
-					continue;//restarts loop
-				}
-		
-		
-				if (strselection == haw) {
-					System.out.println("w");//creates white space
 					
-				}else if (strselection == "o") { 
+				}else if (strselection.equalsIgnoreCase("o")) { 
+					num.showOnesplace(selection);//runs method from other file to show the ones place
+					System.out.println("");//creates white space
 					
-				}else if (strselection == "t") { 
 					
-				}else if (strselection == "h") { 
+				}else if (strselection.equalsIgnoreCase("t")) { 
+					num.showTensplace(selection);//runs method from other file to show the ones place
+					System.out.println("");//creates white space
 					
-				}else if (strselection == "q") { 
+				}else if (strselection.equalsIgnoreCase("h")) { 
+					num.showHundredsplace(selection);//runs method from other file to show the ones place
+					System.out.println("");//creates white space
 					
+				}else if (strselection.equalsIgnoreCase("q")) { 
+					System.out.println("Have a good day.");
+					break;
 				}else {
-					System.out.println("bah");
+					System.out.println("Error. Please select a listed option.");//tells user they made a mistake and to redo input
+					System.out.println("");//white space
 				}
-				
 				
 				
 			} catch(Exception e) {
-				
+				System.out.println("Something went wrong. Please try again.");//tells user to try again
+				System.out.println("");//creates white space
+				continue;//restarts loop
 			}
 			
-			
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		}
 	
 	}
+	
 }
+
+
+
+/*
+
+
+
+test case 1
+
+Enter an integer.
+123
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+w
+The whole number is 123.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+o
+The ones place number is 3.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+n
+Error. Please select a listed option.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+t
+The tens place number is 2.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+h
+The hundreds place number is 1.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+q
+Have a good day.
+
+
+
+
+test case 2 
+
+Enter an integer.
+523
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+w
+The whole number is 523.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+o
+The ones place number is 3.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+t
+The tens place number is 2.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+q
+Have a good day.
+
+
+
+
+test case 3 
+
+
+Enter an integer.
+724
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+h
+The hundreds place number is 7.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+t
+The tens place number is 2.
+
+Show (W)hole number.
+Show (O)nes place number.
+Show (T)ens place number.
+Show (H)undreds place number.
+(Q)uit
+Enter your choice.
+q
+Have a good day.
+
+
+
+
+*/
+
