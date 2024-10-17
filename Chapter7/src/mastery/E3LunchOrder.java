eb00144f8f3ccd87decf71f58779c6f01c78a624
78353001 <78353001@L815WD220031.edu.cbe.ab.ca>
2024-10-17 09:18:26 -0600

    9:18

--- Chapter7/src/mastery/E3LunchOrder.java
+++ Chapter7/src/mastery/E3LunchOrder.java
@@ -1,6 +1,20 @@
 package mastery;
 
+import java.text.DecimalFormat;
+
+/*
+Program: E3LunchOrder.java          Last Date of this Revision: October 17, 2024
+
+Purpose: Create a LunchOrder application that prompts the user for the number of hamburgers, salads, french fries, and sodas and then displays the total for the order.
+
+Author: bilal 
+School: CHHS
+Course: Computer Programming 20
+*/
+
+
 public class E3LunchOrder {
+	DecimalFormat dc = new DecimalFormat("0.00"); // to convert decimals to the nearest hundredth
 	
 	private int hamburgers; //creates private variables
 	private int salads;
@@ -50,46 +64,11 @@ public class E3LunchOrder {
 	
 	
 	
-	public double totalPrice(int burgers,  int salads, int fries, int sodas, double burgersprice, double saladsprice, double friesprice, double sodasprice){
-		double totalprice = (burgers * burgersprice) + (salads * saladsprice) + (fries * friesprice) + (sodas * sodasprice);
-		
-		return (totalprice);
+	public double totalPrice(int burgers,  int salads, int fries, int sodas, double burgersprice, double saladsprice, double friesprice, double sodasprice){ // method to get total price
+		double totalprice = (burgers * burgersprice) + (salads * saladsprice) + (fries * friesprice) + (sodas * sodasprice); //multiples the food item by its price and adds them all together
+		String strtotalprice = (dc.format(totalprice));//rounds to nearest hundreth
+		totalprice = Double.parseDouble(strtotalprice);//changes rounded totalprice from string back to double
+		return (totalprice);//returns the total price of the order
 	}
-	
-	
-/*	
-	public double totalCost(int burger, int salads, int fries, int sodas) 
-	{
-		double totalHam = 
-		
-		
-		return(totalCost);
-	}
-	
-	*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- Chapter7/src/mastery/E3LunchOrderTest.java
+++ Chapter7/src/mastery/E3LunchOrderTest.java
@@ -1,12 +1,25 @@
 package mastery;
 
 
+/*
+Program: E3LunchOrderTest.java          Last Date of this Revision: October 17, 2024
+
+Purpose: Create a LunchOrder application that prompts the user for the number of hamburgers, salads, french fries, and sodas and then displays the total for the order.
+
+Author: bilal 
+School: CHHS
+Course: Computer Programming 20
+*/
+
+
+
+
 
 import java.util.Scanner;//imports scanner
 public class E3LunchOrderTest {
 	public static void main(String[] args){
 	
-		E3LunchOrder hamburger = new E3LunchOrder(1.85, 9, 33, 1); //creates the objects of foodtypes with different prices and foodvalues
+		E3LunchOrder hamburger = new E3LunchOrder(1.85, 9, 33, 1); //creates the objects of food types with different prices and food values
 		E3LunchOrder salad = new E3LunchOrder(2.00, 1, 11, 5);
 		E3LunchOrder fries = new E3LunchOrder(1.30, 11, 36, 4);
 		E3LunchOrder soda = new E3LunchOrder(0.95, 0, 38, 0);
@@ -27,12 +40,12 @@ public class E3LunchOrderTest {
 		
 		
 		while (true) { //loop incase user enters wrong thing
-			try {
+			try { //code will loop back instead of breaking in case of error
 				System.out.println("Enter number of hamburgers."); 
 				String strselection = scanner.nextLine(); //accepts string from user
 				
-				try { //so the code doesnt just break and end
-					selection = Integer.parseInt(strselection);
+				try { //code will loop back instead of breaking in case of error
+					selection = Integer.parseInt(strselection);//changes input from string to int
 				} catch (Exception e) { //catches an error
 					System.out.println("Something went wrong. Please try again.");//tells user to try again
 					System.out.println("");//creates white space
@@ -51,12 +64,12 @@ public class E3LunchOrderTest {
 			
 			
 			while (true) { //loop incase user enters wrong thing
-				try {
+				try { //code will loop back instead of breaking in case of error
 					System.out.println("Enter number of salads."); 
 					String strselection = scanner.nextLine(); //accepts string from user
 					
 					try { //so the code doesnt just break and end
-						selection = Integer.parseInt(strselection);
+						selection = Integer.parseInt(strselection);//changes input from string to int
 					} catch (Exception e) { //catches an error
 						System.out.println("Something went wrong. Please try again.");//tells user to try again
 						System.out.println("");//creates white space
@@ -75,12 +88,12 @@ public class E3LunchOrderTest {
 			
 			
 			while (true) { //loop incase user enters wrong thing
-				try {
+				try { //code will loop back instead of breaking in case of error
 					System.out.println("Enter number of fries."); 
 					String strselection = scanner.nextLine(); //accepts string from user
 					
 					try { //so the code doesnt just break and end
-						selection = Integer.parseInt(strselection);
+						selection = Integer.parseInt(strselection);//changes input from string to int
 					} catch (Exception e) { //catches an error
 						System.out.println("Something went wrong. Please try again.");//tells user to try again
 						System.out.println("");//creates white space
@@ -98,12 +111,12 @@ public class E3LunchOrderTest {
 			}
 			
 			while (true) { //loop incase user enters wrong thing
-				try {
+				try { //code will loop back instead of breaking in case of error
 					System.out.println("Enter number of soda."); 
 					String strselection = scanner.nextLine(); //accepts string from user
 					
 					try { //so the code doesnt just break and end
-						selection = Integer.parseInt(strselection);
+						selection = Integer.parseInt(strselection);//changes input from string to int
 					} catch (Exception e) { //catches an error
 						System.out.println("Something went wrong. Please try again.");//tells user to try again
 						System.out.println("");//creates white space
@@ -122,34 +135,76 @@ public class E3LunchOrderTest {
 			
 			double totalCost = spot.totalPrice(Qburgers, Qsalads, Qfries, Qsoda, hamburger.showPrice(), salad.showPrice(), fries.showPrice(), soda.showPrice()); //runs method  totalprice to get total price
 			System.out.println("Your order comes to: $" + totalCost); //prints final cost
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 			
 		}
 }
+
+
+
+
+/*
+test case 1
+
+Enter number of hamburgers.
+19
+Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0g of fiber.
+Enter number of salads.
+257
+Each salad has 1.0g of fat, 11.0g of carbs, and 5.0g of fiber.
+Enter number of fries.
+1
+Each fries has 11.0g of fat, 36.0g of carbs, and 4.0g of fiber.
+Enter number of soda.
+0
+Each soda has 0.0g of fat, 38.0g of carbs, and 0.0g of fiber.
+Your order comes to: $550.5
+
+
+
+test case 2
+
+Enter number of hamburgers.
+15
+Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0g of fiber.
+Enter number of salads.
+23
+Each salad has 1.0g of fat, 11.0g of carbs, and 5.0g of fiber.
+Enter number of fries.
+w
+Something went wrong. Please try again.
+
+Enter number of fries.
+7
+Each fries has 11.0g of fat, 36.0g of carbs, and 4.0g of fiber.
+Enter number of soda.
+1
+Each soda has 0.0g of fat, 38.0g of carbs, and 0.0g of fiber.
+Your order comes to: $83.8
+
+
+
+test case 3
+
+Enter number of hamburgers.
+78
+Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0g of fiber.
+Enter number of salads.
+w
+Something went wrong. Please try again.
+
+Enter number of salads.
+5
+Each salad has 1.0g of fat, 11.0g of carbs, and 5.0g of fiber.
+Enter number of fries.
+1
+Each fries has 11.0g of fat, 36.0g of carbs, and 4.0g of fiber.
+Enter number of soda.
+5
+Each soda has 0.0g of fat, 38.0g of carbs, and 0.0g of fiber.
+Your order comes to: $160.35
+
+
+*/
+
+
